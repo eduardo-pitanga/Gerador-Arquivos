bddeafe7c4b4189bea4cf18d6a7afe26d322bd2f
eduardo-pitanga <edudupilo@gmail.com>
2024-10-28 23:33:06 -0300

    não ta funcionando

--- src/GeradorDeAlunos.java
+++ src/GeradorDeAlunos.java
@@ -1,31 +1,18 @@
 import com.github.javafaker.Faker;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.ObjectOutputStream;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Locale;
 import java.util.Random;
 
 public class GeradorDeAlunos {
     public static void main(String[] args) {
-        Faker faker = new Faker();
+        Faker faker = new Faker(new Locale("pt-BR"));
         Random random = new Random();
         BufferedWriter buffwrite = null;
-        String registro;
+        String entrada;
 
-        int quantidade_alunos = 10;
-        List<Aluno> alunos = new ArrayList<>();
-
-        for (int j = 0; j < quantidade_alunos; j++) {
-            String nome = faker.name().fullName();
-            String matricula = faker.idNumber().valid();
-            int nota = random.nextInt(101); // Nota entre 0 e 100
-            int frequencia = random.nextInt(101); // Frequência entre 0 e 100
-
-            alunos.add(new Aluno(nome, matricula, nota, frequencia));
-        }
+        int quantidade_alunos = 1000000;
 
         // Escreve os alunos em um arquivo dat
         String nomeArquivo = "alunos.dat";
@@ -34,19 +21,28 @@ public class GeradorDeAlunos {
         } catch (IOException e) {
             System.out.println("erro ao abrir buffer");
         }
-        // escrevendo nos buffers
-        for (Aluno aluno : alunos) {
-            String entrada = aluno.getNome() + "," + aluno.getMatricula() + "," + aluno.getNota() + "," + aluno.getFrequencia() + "\n";
+
+        boolean condicao = true;
+        for (int j = 0; j < quantidade_alunos; j++) {
+            String nome = faker.name().fullName();
+            String matricula = faker.idNumber().valid();
+            int nota = random.nextInt(101); // Nota entre 0 e 100
+            int frequencia = random.nextInt(101); // Frequência entre 0 e 100
+
+            // escrevendo nos buffers
+            entrada = nome + "," + matricula + "," + nota + "," + frequencia + "\n";
             try {
                 assert buffwrite != null;
                 buffwrite.write(entrada);
             } catch (IOException e) {
+                condicao = false;
                 System.out.println("erro ao escrever pelo buffer");
             }
         }
+
+        if (condicao) System.out.println("Arquivo '" + nomeArquivo + "' gerado com sucesso!");
         // fechando buffer
         try {
-            assert buffwrite != null;
             buffwrite.close();
         } catch (IOException e) {
             throw new RuntimeException(e);
